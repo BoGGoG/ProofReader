9a2cb0852697e7d0be5d4148baeb19e0f1cb8940
BoGGoG <marco.knipfer@me.com>
2019-03-12 12:25:20 +0100

    v0.1 working

--- tests/unit/proofreader/test_proofread.py
+++ tests/unit/proofreader/test_proofread.py
@@ -3,15 +3,17 @@
 import unittest
 from unittest.mock import patch, Mock, MagicMock
 
+@patch("proofreader.proofread.print")
 @patch("proofreader.proofread.sys.argv")
 @patch("proofreader.proofread.Netspeak")
 class ProofreadExe(unittest.TestCase):
-    def test_main_creates_Netspeak_instance(self, Netspeak, argv):
+    def test_main_creates_Netspeak_instance(self, Netspeak, argv, mprint):
         from proofreader.proofread import main
         main()
         Netspeak.assert_called_once_with()
 
-    def test_main_calls_Netspeak_instance_with_first_cl_argument(self, Netspeak, argv):
+    def test_main_calls_Netspeak_instance_with_first_cl_argument(
+            self, Netspeak, argv, mprint):
         from proofreader.proofread import main
         netspeak_instance = Mock()
         Netspeak.return_value = netspeak_instance
@@ -23,18 +25,7 @@ class ProofreadExe(unittest.TestCase):
         main()
         netspeak_instance.assert_called_once_with(item)
 
-    # def test_result_of_instance_call_returned_to_user(self, Netspeak, argv):
-        # from proofreader.proofread import main
-        # instance = Mock()
-        # Netspeak.return_value = instance
-
-        # return_value = Mock()
-        # instance.return_value = return_value
-        # main_return = main()
-        # self.assertEqual(main_return, return_value)
-        
-    @patch("proofreader.proofread.print")
-    def test_main_calls_print(self, mprint, Netspeak, argv):
+    def test_main_calls_print(self, Netspeak, argv, mprint):
         from proofreader.proofread import main
         netspeak_instance = Mock()
         Netspeak.return_value = netspeak_instance
@@ -42,9 +33,6 @@ class ProofreadExe(unittest.TestCase):
         netspeak_instance.return_value = query
         main()
         mprint.assert_called_once_with(query)
-        print("banana")
-
-
         
 
 if __name__ == "__main__":
